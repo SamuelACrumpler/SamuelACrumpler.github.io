e495ea528100a59e877e30c6fcea0f58534a5a67
SamuelACrumpler <samuel.a.crumpler@gmail.com>
2020-07-27 19:44:52 -0700

    added WGU Capstone

---
+++ wgucapstone/precache-manifest.ed8ea11a9d13e075f6fa9301ad321917.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45bf16e5b2098f87b7629bcdae44bf24",
+    "url": "/wgucapstone/index.html"
+  },
+  {
+    "revision": "da153bf44dddadb6a71b",
+    "url": "/wgucapstone/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b57f9015b139e3d33425",
+    "url": "/wgucapstone/static/css/main.10559651.chunk.css"
+  },
+  {
+    "revision": "da153bf44dddadb6a71b",
+    "url": "/wgucapstone/static/js/2.b3619667.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/wgucapstone/static/js/2.b3619667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b57f9015b139e3d33425",
+    "url": "/wgucapstone/static/js/main.10ecdef9.chunk.js"
+  },
+  {
+    "revision": "b6268e0e6ebc9c3b62ff",
+    "url": "/wgucapstone/static/js/runtime-main.ae7bc8fd.js"
+  }
+]);
